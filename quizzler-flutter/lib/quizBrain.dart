8bab3c6718388739e0835325faa8c2fbdd838156
Shubham kumar <meshubhama01@gmail.com>
2020-10-05 13:09:57 +0530

    Update quizBrain.dart
    added questions

--- quizzler-flutter/lib/quizBrain.dart
+++ quizzler-flutter/lib/quizBrain.dart
@@ -10,11 +10,11 @@ class QuizBrain{
   List<Icon> scoreKeeper = [];
   List <Question> _questionBank = [
     Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('chelsea will play UCL next season', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
     Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question('Classicdude is an awesome username', true),
     Question('No piece of square dry paper can be folded in half more than 7 times.', false),
     Question('In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.', true),
     Question('The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.', false),
